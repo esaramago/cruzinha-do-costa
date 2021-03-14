ca2791d13131b15f90421f5ff75ddcb6869679ea
Emanuel Saramago <esaramago@slbenfica.pt>
2021-03-14 17:42:16 +0000

    RT firestore

--- public/main.js
+++ public/main.js
@@ -1,21 +1,23 @@
 const App = {
     MAX_RT: 2,
     MAX_CASES: 240,
-    rt: 0.8,
+    rt: null,
     cases: null,
 
+
+    // GET CASES
     _setCases() {
-        const cases = document.getElementById('cases');
+        const cases = document.getElementById("cases");
         if (cases)
             cases.textContent = this.MAX_CASES / 2;
     },
     _getYPosition() {
-        return (this.cases * 90) / this.MAX_CASES + '%';
+        return (this.cases * 90) / this.MAX_CASES + "%";
     },
     _getXPosition() {
-        return (this.rt * 93.5) / this.MAX_RT + '%';
+        return (this.rt * 93.5) / this.MAX_RT + "%";
     },
-    _getData(dates) {
+    _getCases(dates) {
         const startDateStr = this._getFormatedDate(dates.startDate);
         const endDateStr = this._getFormatedDate(dates.endDate);
         const url = `https://covid19-api.vost.pt/Requests/get_entry/${startDateStr}_until_${endDateStr}`;
@@ -29,14 +31,14 @@ const App = {
                 const previousDayCases = commits.confirmados[Object.keys(commits.confirmados)[0]];
                 this.cases = this._calculateCases(lastDayCases, previousDayCases);
                 
-                this._setPosition();
+                this._setPosition("y");
 
             }).catch(err => {
                 // if it doensn't find data, try previous days
                 if (prevDays <= 5) {
                     prevDays ++;
                     var dates = this._getDates(prevDays);
-                    this._getData(dates);
+                    this._getCases(dates);
                 }
             });
 
@@ -59,26 +61,53 @@ const App = {
         return newCasesByPopulation;
     },
     _getFormatedDate(date) {
-        const dateStr = date.toISOString().split('T');
-        const splitDate = dateStr[0].split('-');
+        const dateStr = date.toISOString().split("T");
+        const splitDate = dateStr[0].split("-");
         return `${splitDate[2]}-${splitDate[1]}-${splitDate[0]}`;
     },
-    _setPosition() {
+    _setPosition(axis) {
 
-        const cruzinha = document.getElementById('cruzinha');
+        const cruzinha = document.getElementById("cruzinha");
         if(cruzinha) {
-            cruzinha.style.bottom = this._getYPosition();
-            cruzinha.style.left = this._getXPosition();
+            if (axis === "x") {
+                cruzinha.style.left = this._getXPosition();
+            }
+            if (axis === "y") {
+                cruzinha.style.bottom = this._getYPosition();
+            }
 
-            cruzinha.removeAttribute("hidden");
+            // check if RT and Cases data is loaded
+            if(this.rt && this.cases) {
+                cruzinha.removeAttribute("hidden"); // show cruzinha
+            }
         }
     },
 
+    // GET RT
+    _getRT() {
+
+        // https://console.firebase.google.com/project/cruzinhadocosta/firestore/
+        const db = firebase.initializeApp({ projectId: "cruzinhadocosta" }).firestore();
+
+        db.collection("rt").get()
+            .then(snapshot => {
+                snapshot.forEach(doc => {
+                    const data = doc.data();
+
+                    this.rt = data.rt; // set RT
+                    this._setPosition("x");
+                })
+            })
+    },
+
+    // INIT
     init() {
         this._setCases();
 
+        this._getRT();
+
         const dates = this._getDates();
-        this._getData(dates);
+        this._getCases(dates);
     }
 }
 
